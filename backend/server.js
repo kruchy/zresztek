395a28225b0548b646aa354e704b6be7336c15df
Damian <kontakt@dszymczuk.pl>
2023-05-05 21:18:02 +0200

    add missing body parser

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 const express = require("express");
+const bodyParser = require("body-parser");
 const cors = require("cors");
 const app = express();
 const port = process.env.PORT;
@@ -8,6 +9,10 @@ const asyncErrorHandler = require("./middlewares/asyncErrorHandler");
 const prepareRecipesHandler = require("./prepareRecipes");
 
 app.use(cors());
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(bodyParser.json());
 
 app.use(errorHandling);
 
