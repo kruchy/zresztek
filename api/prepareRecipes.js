82f2e0f8c787906e3625053649ceafc2089da625
krzysiek <kruchy61@gmail.com>
2023-05-05 00:58:56 -0700

    Use chat completion func

--- api/prepareRecipes.js
+++ api/prepareRecipes.js
@@ -1,5 +1,5 @@
 require("dotenv").config();
-const devResponse = require("./response.json")
+const devResponse = require("./response.json");
 
 const axios = require("axios");
 
@@ -10,7 +10,6 @@ const openai = axios.create({
     "Authorization": `Bearer ${process.env.OPENAI_API_KEY}`,
   },
 });
-
 
 async function createChatCompletion(messages, options = {}) {
   try {
@@ -26,47 +25,33 @@ async function createChatCompletion(messages, options = {}) {
   }
 }
 
-module.exports =  async function prepareRecipesHandler(req, res) {
+module.exports = async function prepareRecipesHandler(req, res) {
   try {
-    if(req.body && req.body.ingredients.length === 0){
-      throw new Error('Ingredients are empty');
+    if (req.body && req.body.ingredients.length === 0) {
+      throw new Error("Ingredients are empty");
     }
     const ingredients = req.body.ingredients;
-    
 
-    // const messages = [
-    //   { role: "user", content: `${process.env.RECIPE_PROMPT}: ${ingredients.join(", ")}`, },
-    // ];
-
-    // const options = {
-    //   temperature: 0.8,
-    //   max_tokens: 3000,
-    // };
-
-    // const choices = await createChatCompletion(messages, options);
-
-    const isDev = process.env.DEV_ENABLED === "true"
-    const response = isDev ? { data: devResponse } : await axios.post(
-      "https://api.openai.com/v1/chat/completions",
+    const messages = [
       {
-        model: "gpt-4",
-        messages: [ 
-          {content: `${process.env.RECIPE_PROMPT}: ${ingredients.join(", ")}`,
-          role: "system"}
-        ],
-        max_tokens: 3000,
-        n: 1,
-        temperature: 0.7,
+        role: "system",
+        content: `${process.env.RECIPE_PROMPT}: ${ingredients.join(", ")}`,
       },
-      {
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
-        },
-      }
-    );
+    ];
 
-    const resultResponse = isDev ? response.data.choices[0].message.content : JSON.parse(response.data.choices[0].message.content) 
+    const options = {
+      model: "gpt-4",
+      temperature: 0.7,
+      max_tokens: 3000,
+      n: 1,
+    };
+
+    const isDev = process.env.DEV_ENABLED === "true";
+    const choices = isDev
+      ? devResponse.choices
+      : await createChatCompletion(messages, options);
+
+    const resultResponse = JSON.parse(choices[0].message.content);
 
     const generatedRecipes = resultResponse.map((choice) => {
       const recipe = choice.recipe;
@@ -75,13 +60,13 @@ module.exports =  async function prepareRecipesHandler(req, res) {
       return {
         title,
         recipe,
-        ingredients
+        ingredients,
       };
     });
 
     res.status(200).json(generatedRecipes);
   } catch (error) {
     console.error(error);
-    res.status(500).json({ message: "Failed to generate recipes"});
+    res.status(500).json({ message: "Failed to generate recipes" });
   }
-}
+};
